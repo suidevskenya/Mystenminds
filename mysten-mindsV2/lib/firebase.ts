2d814bfeb1d6db1d1ca85d75f95eb94abb8b0565
Ashley Mwende <ashleymwende24@gmail.com>
2025-05-23 00:36:49 +0300

    Add Realtime database configuration

--- mysten-mindsV2/lib/firebase.ts
+++ mysten-mindsV2/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 import { getAuth } from "firebase/auth"
 
 // Your Firebase configuration
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
+const storage = getStorage(app)
 
-export { app, db, auth }
+export { app, db, auth, storage }
